0272ecbd5877d288da29356fc2757686698ffced
BegoCaZa <begoxcz@hotmail.com>
2025-05-12 14:10:45 +0200

    estoy perdida

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import { useState } from 'react';
 import { v4 } from 'uuid';
 
+const tasksList = [
+  {
+    completed: false,
+    id: v4(),
+    task: 'Make a to do list',
+    img: './assets/icon-cross.svg'
+  }
+];
+
+const completeTask = complete => {
+  if (complete) {
+    tasksList.completed = true;
+  }
+};
+
 const App = () => {
-  const tasksList = [];
+  const [complete, setComplete] = useState(false);
+  console.log(complete);
 
   return (
     <div className='general-container'>
@@ -23,30 +39,39 @@ const App = () => {
           />
         </form>
 
+        {/* dibujar cada tarea */}
         <div className='to-do-list'>
-          <div className='task-container'>
-            <input className='checkbox' type='checkbox' id='checkbox' />
-            <label for='checkbox' class='task-label'>
-              Make a to do list
-            </label>
-            <img
-              id='delete-button'
-              class='delete-button'
-              src='./assets/icon-cross.svg'
-              alt='delete button'
-            />
-          </div>
+          {tasksList.map(task => (
+            <div key={task.id} className='task-container'>
+              <input
+                className='checkbox'
+                type='checkbox'
+                id={task.id}
+                checked={task.completed}
+                onChange={() => setComplete(!complete)}
+              />
+              <label htmlFor={task.id} className='task-label'>
+                {task.task}
+              </label>
+              <img
+                id='delete-button'
+                className='delete-button'
+                src={task.img}
+                alt='delete button'
+              />
+            </div>
+          ))}
         </div>
 
-        <div class='list-footer'>
+        <div className='list-footer'>
           <span>0 items left</span>
           <span>Clear Completed</span>
         </div>
 
-        <div class='filters'>
-          <button class='filter filter-active'>All</button>
-          <button class='filter'>Active</button>
-          <button class='filter'>Completed</button>
+        <div className='filters'>
+          <button className='filter filter-active'>All</button>
+          <button className='filter'>Active</button>
+          <button className='filter'>Completed</button>
         </div>
       </main>
     </div>
