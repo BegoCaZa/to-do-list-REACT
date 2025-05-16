7a170e18d49d453464b4bbd7e2032be3283203ad
BegoCaZa <begoxcz@hotmail.com>
2025-05-16 21:25:26 +0200

    ii

--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -22,33 +22,6 @@ const Filters = ({ filteredTasks, filter, setFilter }) => {
             {option}
           </button>
         ))}
-
-        {/* //   <button
-        {/* <button
-          className={`${styles.filter} ${
-            filter === 'all' && `${styles.filterActive}`
-          }`}
-          //   operador binario--cuando el ELSE no es NADA
-          onClick={() => setFilter('all')}
-        >
-          All
-        </button>
-        <button
-          className={`${styles.filter} ${
-            filter === 'active' && `${styles.filterActive}`
-          }`}
-          onClick={() => setFilter('active')}
-        >
-          Active
-        </button>
-        <button
-          className={`${styles.filter} ${
-            filter === 'completed' && `${styles.filterActive}`
-          }`}
-          onClick={() => setFilter('completed')}
-        >
-          Completed
-        </button> */}
       </div>
     </>
   );
